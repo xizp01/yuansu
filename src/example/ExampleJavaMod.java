fbef5b723e102f200fd631168c9961a629dfc325
xizp01 <892580636@qq.com>
2026-08-12 19:00:46 +0800

    更新 ExampleJavaMod.java

--- src/example/ExampleJavaMod.java
+++ src/example/ExampleJavaMod.java
@@ -2,26 +2,12 @@ package example;
 
 import arc.*;
 import arc.util.*;
-import mindustry.game.EventType.*;
 import mindustry.mod.*;
-import mindustry.ui.dialogs.*;
 
 public class ExampleJavaMod extends Mod{
 
     public ExampleJavaMod(){
         Log.info("Loaded ExampleJavaMod constructor.");
-
-        //监听游戏加载完成事件
-        Events.on(ClientLoadEvent.class, e -> {
-            Time.runTask(10f, () -> {
-                BaseDialog dialog = new BaseDialog("frog");
-                dialog.cont.add("behold").row();
-                //资源ID 和mod.hjson的name严格对应 yuansu-mod-frog
-                dialog.cont.image(Core.atlas.find("yuansu-mod-frog")).pad(20f).row();
-                dialog.cont.button("I see", dialog::hide).size(100f, 50f);
-                dialog.show();
-            });
-        });
     }
 
     @Override
