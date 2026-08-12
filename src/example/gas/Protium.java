9dda2655dcf0ce31e91c854b938ec858b098dc50
xizp01 <892580636@qq.com>
2026-08-12 23:53:08 +0800

    修复缺少导入包编译错误

--- src/example/gas/Protium.java
+++ src/example/gas/Protium.java
@@ -1,27 +1,22 @@
-package example.gas;
+package example;
 
-import arc.graphics.Color;
-import mindustry.type.Gas;
+import arc.util.*;
+import example.gas.Protium;
+import mindustry.mod.*;
 
-public class Protium extends Gas {
-    public Protium(){
-        super("protium");
+public class Yuansu extends Mod{
+    public static Protium protium;
 
-        localizedName = "氕气";
-        description = """
-宇宙中含量最高的氢同位素气体，由¹H₂分子组成。
-高度易燃，遇氧气能够剧烈爆燃。
-无法维持核聚变反应，不可作为聚变燃料。
-原子核不含中子，完全没有放射性。
-        """;
+    public Yuansu(){
+        Log.info("Loaded Yuansu constructor.");
+    }
 
-        color = Color.valueOf("#c8e8ff");
-        gasColor = Color.valueOf("#e6f4ff");
-        barColor = Color.valueOf("#c8e8ff");
+    @Override
+    public void loadContent(){
+        Log.info("Loading some example content.");
 
-        flammability = 1.3f;
-        explosiveness = 1.2f;
-        viscosity = 0.45f;
-        temperature = 25f;
+        //注册氕气
+        protium = new Protium();
+        protium.load();
     }
 }
