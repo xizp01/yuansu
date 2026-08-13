734220f9f946d43f4ee60e0342aaece30f4e8198
xizp01 <892580636@qq.com>
2026-08-13 18:56:45 +0800

    回调

--- src/example/Yuansu.java
+++ src/example/Yuansu.java
@@ -1,7 +1,6 @@
 package example;
 
 import arc.util.Log;
-import example.liquid.Protium;
 import mindustry.mod.Mod;
 
 public class Yuansu extends Mod{
@@ -11,7 +10,6 @@ public class Yuansu extends Mod{
     public void loadContent(){
         Log.info("【元素模组】开始加载内容");
         protium = new Protium();
-        protium.register(); //核心修改
-        Log.info("氕气(protium)注册成功！");
+        protium.load();
     }
 }
