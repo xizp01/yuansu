97a6397d2519c1e5247abf63a7a98d5ea3c3c239
ganm <ganm@local>
2026-08-31 11:06:30 +0800

    电解制氢机改为同时输出氢气和氧气(2:1)

--- src/ganm/content/blocks/SerpuloElectrolyzer.java
+++ src/ganm/content/blocks/SerpuloElectrolyzer.java
@@ -1,46 +1,16 @@
 package ganm.content.blocks;
 
-import mindustry.type.Category;
-import mindustry.type.ItemStack;
-import mindustry.type.LiquidStack;
 import mindustry.world.Block;
-import mindustry.world.blocks.production.GenericCrafter;
-import mindustry.content.Items;
-import mindustry.content.Liquids;
-import mindustry.content.Planets;
-import mindustry.content.Fx;
 
 /**
  * 塞普罗电解制氢机
- * 通过电解水制取氢气，为塞普罗同位素分离产业链提供原料。
+ * 通过电解水同时制取氢气和氧气，为塞普罗同位素分离产业链提供原料。
  * 所属星球：塞普罗
  */
 public class SerpuloElectrolyzer {
     public static Block block;
 
     public static void load() {
-        block = new GenericCrafter("serpulo-electrolyzer") {{
-            requirements(Category.crafting, ItemStack.with(
-                Items.copper, 40,
-                Items.lead, 30,
-                Items.titanium, 15,
-                Items.silicon, 20
-            ));
-            size = 2;
-            health = 200;
-            craftTime = 60f;
-            hasPower = true;
-            hasLiquids = true;
-            liquidCapacity = 15f;
-            outputLiquid = new LiquidStack(Liquids.hydrogen, 5f);
-            consumeLiquid(Liquids.water, 10f);
-            consumePower(2.0f);
-            shownPlanets.add(Planets.serpulo);
-            craftEffect = Fx.vapor;
-            updateEffect = Fx.steam;
-            updateEffectChance = 0.1f;
-            updateEffectSpread = 6f;
-            warmupSpeed = 0.03f;
-        }};
+        block = new SerpuloElectrolyzerBlock("serpulo-electrolyzer");
     }
 }
---
+++ src/ganm/content/blocks/SerpuloElectrolyzerBlock.java
@@ -0,0 +1,48 @@
+package ganm.content.blocks;
+
+import mindustry.gen.*;
+import mindustry.type.*;
+import mindustry.world.blocks.production.*;
+import mindustry.content.*;
+
+/**
+ * 塞普罗电解制氢机（自定义方块类）
+ * 电解水同时产出氢气和氧气（比例 2:1）。
+ */
+public class SerpuloElectrolyzerBlock extends GenericCrafter {
+    public float oxygenOutput = 2.5f;
+
+    public SerpuloElectrolyzerBlock(String name) {
+        super(name);
+        requirements(Category.crafting, ItemStack.with(
+            Items.copper, 40,
+            Items.lead, 30,
+            Items.titanium, 15,
+            Items.silicon, 20
+        ));
+        size = 2;
+        health = 200;
+        craftTime = 60f;
+        hasPower = true;
+        hasLiquids = true;
+        liquidCapacity = 30f;
+        outputLiquid = new LiquidStack(Liquids.hydrogen, 5f);
+        consumeLiquid(Liquids.water, 10f);
+        consumePower(2.0f);
+        shownPlanets.add(Planets.serpulo);
+        craftEffect = Fx.vapor;
+        updateEffect = Fx.steam;
+        updateEffectChance = 0.1f;
+        updateEffectSpread = 6f;
+        warmupSpeed = 0.03f;
+    }
+
+    public class SerpuloElectrolyzerBuild extends GenericCrafterBuild {
+        @Override
+        public void craft() {
+            super.craft();
+            // 水电解：2H2O → 2H2 + O2，同时产出氧气
+            liquids.add(Liquids.oxygen, oxygenOutput);
+        }
+    }
+}
