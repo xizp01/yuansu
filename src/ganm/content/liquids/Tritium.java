db37933f1d7c3800ee3c8032af7d6925e92838ac
ganm <ganm@local>
2026-09-03 19:22:08 +0800

    氚气关联辐射状态效果：effect=Radiation.effect，接触氚气的单位会被施加辐射

--- src/ganm/content/liquids/Tritium.java
+++ src/ganm/content/liquids/Tritium.java
@@ -1,18 +1,16 @@
 package ganm.content.liquids;
-
 import arc.graphics.Color;
 import mindustry.type.Liquid;
 import mindustry.content.Planets;
-
+import ganm.content.status.Radiation;
 /**
  * 氚气（超重氢）
  * 氢的放射性同位素，核聚变的关键燃料，半衰期约12.3年。
- * 具有放射性，其分离机周围会产生辐射区域。
+ * 具有放射性，接触单位会施加辐射状态。
  * 所属星球：埃里克尔、塞普罗
  */
 public class Tritium {
     public static Liquid liquid;
-
     public static void load() {
         liquid = new Liquid("tritium", Color.valueOf("5a6ab0")) {{
             gas = true;
@@ -20,7 +18,7 @@ public class Tritium {
             viscosity = 0.14f;
             temperature = 0.25f;
             heatCapacity = 0.4f;
-            radioactivity = 1.5f;
+            effect = Radiation.effect;
             barColor = Color.valueOf("3a4a90");
             shownPlanets.add(Planets.erekir);
             shownPlanets.add(Planets.serpulo);
